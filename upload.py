55beefc5164d5f1dfac34ea78ee0da16bc70230c
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-24 09:45:52 +0000

    upload script

---
+++ upload.py
@@ -0,0 +1,7 @@
+from pecha_uploader.pipeline import upload
+from pecha_uploader.config import Destination_url
+
+from utils import read_json
+
+input = read_json("jsons/sherab/root/pecha_display.json")
+upload(input, Destination_url.STAGING, True)
