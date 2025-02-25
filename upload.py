8c4969e7aa40f06e76ee52adf815d0f62bc5a563
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-25 19:30:53 +0000

    save code

--- upload.py
+++ upload.py
@@ -3,5 +3,5 @@ from pecha_uploader.config import Destination_url
 
 from utils import read_json
 
-input = read_json("jsons/sherab/root/sherab_translations.json")
-upload(input, Destination_url.PRODUCTION, True)
+input = read_json("jsons/sherab/root/sherab_en_translations.json")
+upload(input, Destination_url.STAGING, True)
