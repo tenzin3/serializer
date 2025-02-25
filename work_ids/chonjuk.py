46b0f8623c429e7b7e42d44b73a1a5544969de59
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-25 08:38:49 +0000

    save chonjuk sherab work ids

---
+++ work_ids/chonjuk.py
@@ -0,0 +1,24 @@
+
+
+works = [
+         {
+            "root_display_id": "I7C0673C3",
+            "root_id": "I7AF39EB9",
+            "commentary_id": "I105531AD"
+         },
+         {
+            "root_display_id": "I7C0673C3",
+            "root_id": "I1B923993",
+            "commentary_id": "IB714D443"
+         },
+         {
+            "root_display_id": "I7C0673C3",
+            "root_id": "I27D25999",
+            "commentary_id": "I9294E222"
+         }, 
+         {
+            "root_display_id": "I7C0673C3",
+            "root_id": "I5C855E8C",
+            "commentary_id": "IFE85856E"
+         }
+]
---
+++ work_ids/sherab.py
@@ -0,0 +1,17 @@
+
+works = [{
+            "root_display_id": "I8BCEB363",
+            "root_id": "I9943B599",
+            "commentary_id": "I22734F80"
+         },
+         {
+            "root_display_id": "I8BCEB363",
+            "root_id": "I85E31AAE",
+            "commentary_id": "I70BA77E2"
+         },
+         {
+            "root_display_id": "I8BCEB363",
+            "root_id": "I02DEEEA9",
+            "commentary_id": "I7D9965EE"
+         }
+]
