ebb70e80e80f1a10299bcff79938a10ca7acc058
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-25 10:23:25 +0000

    refactor serialize commentary

--- serialize_commentary.py
+++ serialize_commentary.py
@@ -1,29 +1,14 @@
 from pathlib import Path 
 from utils import download_pecha, write_json
+from typing import List, Dict 
 
 from openpecha.pecha import Pecha 
 from openpecha.pecha.serializers.pecha_db.commentary.simple_commentary import SimpleCommentarySerializer
 from alignment_ann_transfer.commentary import CommentaryAlignmentTransfer
 
-works = [{
-            "root_display_id": "I8BCEB363",
-            "root_id": "I9943B599",
-            "commentary_id": "I22734F80"
-         },
-         {
-            "root_display_id": "I8BCEB363",
-            "root_id": "I85E31AAE",
-            "commentary_id": "I70BA77E2"
-         },
-         {
-            "root_display_id": "I8BCEB363",
-            "root_id": "I02DEEEA9",
-            "commentary_id": "I7D9965EE"
-         }
-]
 
-def serialize_commentaries():
-    for work in works:    
+def serialize_commentaries(work_ids: List[Dict], output_path:str):
+    for work in work_ids:    
         commentary_id = work["commentary_id"]
         commentary_pecha = Pecha.from_path(download_pecha(commentary_id, Path("tmp")))
         
@@ -42,8 +27,26 @@ def serialize_commentaries():
         # Strip the tgt content
         tgt_content = [content.strip() for content in tgt_content]
         serialized["target"]["books"][0]["content"] = [tgt_content]
-        write_json(f"jsons/sherab/commentary/{commentary_id}.json", serialized)
+        write_json(f"{output_path}/{commentary_id}.json", serialized)
 
 
 if __name__ == "__main__":
-    serialize_commentaries()
+    sherab_work_ids = [{
+            "root_display_id": "I8BCEB363",
+            "root_id": "I9943B599",
+            "commentary_id": "I22734F80"
+         },
+         {
+            "root_display_id": "I8BCEB363",
+            "root_id": "I85E31AAE",
+            "commentary_id": "I70BA77E2"
+         },
+         {
+            "root_display_id": "I8BCEB363",
+            "root_id": "I02DEEEA9",
+            "commentary_id": "I7D9965EE"
+         }
+    ]
+
+    output_path = "jsons/sherab/commentary"
+    serialize_commentaries(sherab_work_ids, output_path=output_path)
