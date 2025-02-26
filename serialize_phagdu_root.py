835ff14552d1168c8bdf59323ea26dffa40e808e
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-26 06:42:54 +0000

    serialize phagdu

--- serialize_phagdu_root.py
+++ serialize_phagdu_root.py
@@ -2,11 +2,31 @@ import re
 from pathlib import Path 
 from utils import download_pecha, write_json
 from stam import AnnotationStore
-
+from typing import List, Dict 
 from openpecha.pecha import Pecha 
 from openpecha.pecha.serializers.pecha_db.translation import TranslationSerializer
 from utils import read_json
 
+
+
+def group_segments_by_chapter(segments: List[str], chapter_info: Dict) -> List[List[str]]:
+    """
+    Group segments into chapters. A new chapter starts when a segment begins
+    with "ch" (case-insensitive). Segments that come before the first chapter marker
+    will be grouped together.
+    
+    Returns a list of chapters, where each chapter is a list of segments.
+    """
+
+    chapterized_segments = []
+    for chapter_num, segment_indices in chapter_info.items():
+        chapter = []
+        for idx in segment_indices:
+            if idx >= len(segments):
+                break
+            chapter.append(segments[idx-1])
+        chapterized_segments.append(chapter)
+    return chapterized_segments
 
 def serialize_bo_root():
     root_id = "IB83348ED"
@@ -17,8 +37,10 @@ def serialize_bo_root():
 
     segments = [str(ann) for ann in anns]
 
+    chapter_info = read_json("jsons/phagdu/chapter.json")
+    chapterized_segmenents = group_segments_by_chapter(segments, chapter_info )
     serialized = read_json("jsons/phagdu/root/phagdu_root.json")
-    serialized["target"]["books"][0]["content"] = [segments]
+    serialized["target"]["books"][0]["content"] = chapterized_segmenents
     write_json("jsons/phagdu/root/phagdu_root.json", serialized)
     write_json("root_segments.json", segments)
 
